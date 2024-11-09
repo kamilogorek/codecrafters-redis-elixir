e8740147ded5fd05093153ea5fff12331c7ba6f0
Kamil Ogórek <kamil.ogorek@gmail.com>
2024-11-09 18:17:52 +0100

    [#RG2] Respond to PING

--- lib/server.ex
+++ lib/server.ex
@@ -1,3 +1,5 @@
+# watchexec -w lib -r --stop-signal SIGKILL mix run --no-halt
+
 defmodule Server do
   @moduledoc """
   Your implementation of a Redis server
@@ -19,6 +21,32 @@ defmodule Server do
     # Since the tester restarts your program quite often, setting SO_REUSEADDR
     # ensures that we don't run into 'Address already in use' errors
     {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
-    {:ok, _client} = :gen_tcp.accept(socket)
+    accept_connection(socket)
+  end
+
+  def accept_connection(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    receive_packet(client)
+
+    # {:ok, pid} =
+    #   Task.Supervisor.start_child(Server.TaskSupervisor, fn -> receive_packet(client, config) end)
+
+    # :ok = :gen_tcp.controlling_process(client, pid)
+    # accept_connection(socket, config)
+  end
+
+  def receive_packet(client) do
+    case :gen_tcp.recv(client, 0) do
+      {:ok, data} ->
+        IO.inspect("Data: #{data}")
+        :gen_tcp.send(client, "+PONG\r\n")
+        receive_packet(client)
+
+      {:error, :closed} ->
+        nil
+
+      {:error, reason} ->
+        IO.puts("TCP connection failed: #{reason}")
+    end
   end
 end
